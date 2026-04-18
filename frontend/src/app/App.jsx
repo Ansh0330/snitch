cdae7c87b5157e594125efa01c51671ef8110507
Ansh Kotnala <meanshbhardwaj@gmail.com>
2026-04-18 15:10:48 +0530

    feat: install react-router and implement application routing configuration

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
 import './App.css'
-
+import { RouterProvider } from 'react-router'
+import { routes } from   './app.routes'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <h1>Hello World</h1>
+    <RouterProvider router={routes} />
   )
 }
 
